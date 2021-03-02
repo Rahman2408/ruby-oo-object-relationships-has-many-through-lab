e0d73f525dffdc59b02a3d71472559be298573fa
Abdelrahman Elsirafy <rahman2408@gmail.com>
2021-03-02 13:09:15 -0500

    commit tree be2252a2f9f5d39fcceb39c125aa21f8c6c95d60

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,30 @@
 
+class Artist
+    
+    attr_accessor :name, :artist, :genre, :songs, :genres
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self 
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_song(name, genre)
+        Song.new(name ,self, genre)
+        
+    end
+
+    def songs
+        Song.all.select{|song| song.artist == self}
+    end
+
+    def genres
+        songs.map(&:genre)   #not too sure here
+    end
+
+end
+ 
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,28 @@
+class Genre
+        
+    attr_accessor :name, :artist, :songs, :artist
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self 
+    end
+
+    def self.all
+        @@all
+    end
+
+    # def new_song(name, genre)
+    #     Song.new(name)
+    #     Genre.new(genre)
+    #     artist.name = self
+    # end
+
+    def songs
+        Song.all{|song| song.genre == self}
+    end
+
+    def artists
+        songs.map(&:artist)   #not too sure here either
+    end
+end
